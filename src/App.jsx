f28ca3378046b9c2998b0d3695f3d4be313c5ddc
Prashant Thakur <prashant.thakurnp2@gmail.com>
2026-02-27 19:52:35 +0530

    Added Functionality of the Buttons using useState() Hook

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,30 @@ const App = () => {
 
     <div className='flex w-312 h-18 bg-amber-50 rounded-xl mb-12 items-center justify-evenly'>
       
-      <button className='bg-red-500 px-10 h-16 rounded-2xl'>Red</button>
-      <button className='bg-blue-500 px-10 h-16 rounded-2xl'>Blue</button>
-      <button className='bg-green-500 px-10 h-16 rounded-2xl'>Green</button>
-      <button className='bg-yellow-500 px-10 h-16 rounded-2xl'>Yellow</button>
-      <button className='bg-purple-500 px-10 h-16 rounded-2xl'>Purple</button>
-      <button className='bg-pink-500 px-10 h-16 rounded-2xl'>Pink</button>
-      <button className='bg-indigo-500 px-10 h-16 rounded-2xl'>Indigo</button>
-      <button className='bg-orange-500 px-10 h-16 rounded-2xl'>Orange</button>
+      <button className='bg-red-500 px-10 h-16 rounded-2xl' onClick={()=>{
+        setbgcolorbaby('red')
+      }}>Red</button>
+      <button className='bg-blue-500 px-10 h-16 rounded-2xl' onClick={()=>{
+        setbgcolorbaby('blue')
+      }}>Blue</button>
+      <button className='bg-green-500 px-10 h-16 rounded-2xl' onClick={()=>{
+        setbgcolorbaby('green')
+      }}>Green</button>
+      <button className='bg-yellow-500 px-10 h-16 rounded-2xl' onClick={()=>{
+        setbgcolorbaby('yellow')
+      }}>Yellow</button>
+      <button className='bg-purple-500 px-10 h-16 rounded-2xl' onClick={()=>{
+        setbgcolorbaby('purple')
+      }}>Purple</button>
+      <button className='bg-pink-500 px-10 h-16 rounded-2xl' onClick={()=>{
+        setbgcolorbaby('pink')
+      }}>Pink</button>
+      <button className='bg-indigo-500 px-10 h-16 rounded-2xl' onClick={()=>{
+        setbgcolorbaby('indigo')
+      }}>Indigo</button>
+      <button className='bg-orange-500 px-10 h-16 rounded-2xl' onClick={()=>{
+        setbgcolorbaby('orange')
+      }}>Orange</button>
 
     </div>
 
